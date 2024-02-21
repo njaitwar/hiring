973ab567453b412ef09522c5ff5e3a0b501ccb7a
njaitwar <njaitwarg@gmail.com>
2024-02-21 21:46:14 +0530

    hiring process

---
+++ src/main/java/com/divergentsl/NewHiringApplication.java
@@ -0,0 +1,32 @@
+package com.divergentsl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.camunda.bpm.application.PostDeploy;
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+import org.camunda.bpm.engine.ProcessEngine;
+
+/**
+ * Process Application exposing this application's resources the process engine. 
+ */
+@ProcessApplication
+public class NewHiringApplication extends ServletProcessApplication {
+
+  private static final String PROCESS_DEFINITION_KEY = "new_hiring_custom_form_process";
+
+  /**
+   * In a @PostDeploy Hook you can interact with the process engine and access 
+   * the processes the application has deployed. 
+   */
+  @PostDeploy
+  public void onDeploymentFinished(ProcessEngine processEngine) {
+
+    // start an initial process instance
+    Map<String, Object> variables = new HashMap<String, Object>();
+    variables.put("name", "nilesh");
+    processEngine.getRuntimeService().startProcessInstanceByKey(PROCESS_DEFINITION_KEY, variables);
+  }
+
+}
